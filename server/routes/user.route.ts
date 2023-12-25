76515904ee8f84a28c8d314d9bf272cfdc9d520a
dt4real <dotchere95@gmail.com>
2023-12-25 16:42:46 +0100

    feat: Implement User Authentication with Social Media and Email/Password Authentication
    - Add social media authentication for Google and GitHub
    - Integrate email and password authentication
    - Implement OTP messaging for user registration confirmation

---
+++ server/routes/user.route.ts
@@ -0,0 +1,34 @@
+import express from "express";
+import {
+  activateUser,
+  getUserInfo,
+  loginUser,
+  logoutUser,
+  registrationUser,
+  socialAuth,
+  updatePassword,
+  updateProfilePicture,
+  updateUserInfo,
+} from "../controllers/user.controller";
+import { isAutheticated } from "../middleware/auth";
+const userRouter = express.Router();
+
+userRouter.post("/register", registrationUser);
+
+userRouter.post("/activate-user", activateUser);
+
+userRouter.post("/login", loginUser);
+
+userRouter.get("/logout", isAutheticated, logoutUser);
+
+userRouter.get("/me", isAutheticated, getUserInfo);
+
+userRouter.post("/social-auth", socialAuth);
+
+userRouter.put("/update-user-info", isAutheticated, updateUserInfo);
+
+userRouter.put("/update-user-password", isAutheticated, updatePassword);
+
+userRouter.put("/update-user-avatar", isAutheticated, updateProfilePicture);
+
+export default userRouter;
