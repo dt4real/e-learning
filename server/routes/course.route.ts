ff9bd496567785be3ddd027f7e69e28dfca82378
dt4real <dotchere95@gmail.com>
2024-01-07 09:36:58 +0100

    Added route to create, update, get single and get all courses

---
+++ server/routes/course.route.ts
@@ -0,0 +1,33 @@
+import express from "express";
+import {
+	editCourse,
+	getAllCourses,
+	getCourseByUser,
+	getSingleCourse,
+	uploadCourse,
+} from "../controllers/course.controller";
+import { authorizeRoles, isAutheticated } from "../middleware/auth";
+
+const courseRouter = express.Router();
+
+courseRouter.post(
+	"/create-course",
+	isAutheticated,
+	authorizeRoles("admin"),
+	uploadCourse
+);
+
+courseRouter.put(
+	"/edit-course/:id",
+	isAutheticated,
+	authorizeRoles("admin"),
+	editCourse
+);
+
+courseRouter.get("/get-course/:id", getSingleCourse);
+
+courseRouter.get("/get-courses", getAllCourses);
+
+courseRouter.get("/get-course-content/:id", isAutheticated, getCourseByUser);
+
+export default courseRouter;
