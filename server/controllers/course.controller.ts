e37498d11c7eec5f4fcbf4341860c3e1dbfaddd2
dt4real <dotchere95@gmail.com>
2024-01-07 08:55:35 +0100

    feat: Implement course controllers
    Introduce controllers for uploading, editing, and retrieving course data. The controllers handle various aspects, including cloudinary image uploads, Redis caching, and ensuring valid user access to course content. Each controller is wrapped with error handling using the CatchAsyncError middleware.
    
    - `uploadCourse`: Handles course creation with optional thumbnail upload.
    - `editCourse`: Manages the editing of existing courses, updating thumbnails if necessary.
    - `getSingleCourse`: Retrieves details of a single course, utilizing Redis for caching.
    - `getAllCourses`: Retrieves a list of all courses without specific content details.
    - `getCourseByUser`: Provides access to course content only for valid users.

---
+++ server/controllers/course.controller.ts
@@ -0,0 +1,168 @@
+import { NextFunction, Request, Response } from "express";
+import { CatchAsyncError } from "../middleware/catchAsyncErrors";
+import ErrorHandler from "../utils/ErrorHandler";
+import cloudinary from "cloudinary";
+import CourseModel, { IComment } from "../models/course.model";
+import { redis } from "../utils/redis";
+import mongoose from "mongoose";
+import path from "path";
+import ejs from "ejs";
+import sendMail from "../utils/sendMail";
+
+// upload course
+export const uploadCourse = CatchAsyncError(
+	async (req: Request, res: Response, next: NextFunction) => {
+		try {
+			const data = req.body;
+			const thumbnail = data.thumbnail;
+			if (thumbnail) {
+				const myCloud = await cloudinary.v2.uploader.upload(thumbnail, {
+					folder: "courses",
+				});
+
+				data.thumbnail = {
+					public_id: myCloud.public_id,
+					url: myCloud.secure_url,
+				};
+			}
+			const course = await CourseModel.create(data);
+			res.status(201).json({
+				success: true,
+				course
+			});
+		} catch (error: any) {
+			return next(new ErrorHandler(error.message, 500));
+		}
+	}
+);
+
+// edit course
+export const editCourse = CatchAsyncError(
+	async (req: Request, res: Response, next: NextFunction) => {
+		try {
+			const data = req.body;
+
+			const thumbnail = data.thumbnail;
+
+			const courseId = req.params.id;
+
+			const courseData = await CourseModel.findById(courseId) as any;
+
+			if (thumbnail && !thumbnail.startsWith("https")) {
+				await cloudinary.v2.uploader.destroy(courseData.thumbnail.public_id);
+
+				const myCloud = await cloudinary.v2.uploader.upload(thumbnail, {
+					folder: "courses",
+				});
+
+				data.thumbnail = {
+					public_id: myCloud.public_id,
+					url: myCloud.secure_url,
+				};
+			}
+
+			if (thumbnail.startsWith("https")) {
+				data.thumbnail = {
+					public_id: courseData?.thumbnail.public_id,
+					url: courseData?.thumbnail.url,
+				};
+			}
+
+			const course = await CourseModel.findByIdAndUpdate(
+				courseId,
+				{
+					$set: data,
+				},
+				{ new: true }
+			);
+
+			res.status(201).json({
+				success: true,
+				course,
+			});
+		} catch (error: any) {
+			return next(new ErrorHandler(error.message, 500));
+		}
+	}
+);
+
+// get single course --- without purchasing
+export const getSingleCourse = CatchAsyncError(
+	async (req: Request, res: Response, next: NextFunction) => {
+		try {
+			const courseId = req.params.id;
+
+			const isCacheExist = await redis.get(courseId);
+
+			if (isCacheExist) {
+				const course = JSON.parse(isCacheExist);
+				res.status(200).json({
+					success: true,
+					course,
+				});
+			} else {
+				const course = await CourseModel.findById(req.params.id).select(
+					"-courseData.videoUrl -courseData.suggestion -courseData.questions -courseData.links"
+				);
+
+				await redis.set(courseId, JSON.stringify(course), "EX", 604800); // 7days
+
+				res.status(200).json({
+					success: true,
+					course,
+				});
+			}
+		} catch (error: any) {
+			return next(new ErrorHandler(error.message, 500));
+		}
+	}
+);
+
+// get all courses --- without purchasing
+export const getAllCourses = CatchAsyncError(
+	async (req: Request, res: Response, next: NextFunction) => {
+		try {
+			const courses = await CourseModel.find().select(
+				"-courseData.videoUrl -courseData.suggestion -courseData.questions -courseData.links"
+			);
+
+			res.status(200).json({
+				success: true,
+				courses,
+			});
+		} catch (error: any) {
+			return next(new ErrorHandler(error.message, 500));
+		}
+	}
+);
+
+// get course content -- only for valid user
+export const getCourseByUser = CatchAsyncError(
+	async (req: Request, res: Response, next: NextFunction) => {
+		try {
+			const userCourseList = req.user?.courses;
+			const courseId = req.params.id;
+
+			const courseExists = userCourseList?.find(
+				(course: any) => course._id.toString() === courseId
+			);
+
+			if (!courseExists) {
+				return next(
+					new ErrorHandler("You are not eligible to access this course", 404)
+				);
+			}
+
+			const course = await CourseModel.findById(courseId);
+
+			const content = course?.courseData;
+
+			res.status(200).json({
+				success: true,
+				content,
+			});
+		} catch (error: any) {
+			return next(new ErrorHandler(error.message, 500));
+		}
+	}
+);
